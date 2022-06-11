c08556f3927fe858aa4292dd795db710ebe61149
Alexandr Burykin <Oleksandr.Burykin@privatbank.ua>
2022-06-11 18:38:44 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/burykin/weather_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.burykin.weather_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/auth_page/auth_state.dart
@@ -0,0 +1,60 @@
+part of 'auth_cubit.dart';
+
+enum  AuthStatus{initial, success, failure}
+
+class AuthState {
+ final AuthStatus status;
+ final String errorMessage;
+
+//<editor-fold desc="Data Methods">
+
+ const AuthState({
+     this.status = AuthStatus.initial,
+     this.errorMessage = '',
+  });
+
+ @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      (other is AuthState &&
+          runtimeType == other.runtimeType &&
+          status == other.status &&
+          errorMessage == other.errorMessage);
+
+ @override
+  int get hashCode => status.hashCode ^ errorMessage.hashCode;
+
+ @override
+  String toString() {
+    return 'AuthState{' +
+        ' status: $status,' +
+        ' errorMessage: $errorMessage,' +
+        '}';
+  }
+
+ AuthState copyWith({
+    AuthStatus? status,
+    String? errorMessage,
+  }) {
+    return AuthState(
+      status: status ?? this.status,
+      errorMessage: errorMessage ?? this.errorMessage,
+    );
+  }
+
+ Map<String, dynamic> toMap() {
+    return {
+      'status': this.status,
+      'errorMessage': this.errorMessage,
+    };
+  }
+
+  factory AuthState.fromMap(Map<String, dynamic> map) {
+    return AuthState(
+      status: map['status'] as AuthStatus,
+      errorMessage: map['errorMessage'] as String,
+    );
+  }
+
+//</editor-fold>
+}
