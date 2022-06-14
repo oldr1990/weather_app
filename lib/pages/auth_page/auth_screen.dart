68ad6e185d914c11988a7738defd467bedc94d5a
oldr1990 <sashik4281@gmail.com>
2022-06-14 10:12:04 +0300

    add validation and upgrade gradle
    Signed-off-by: oldr1990 <sashik4281@gmail.com>

--- lib/pages/auth_page/auth_screen.dart
+++ lib/pages/auth_page/auth_screen.dart
@@ -18,20 +18,50 @@ class _AuthScreenState extends State<AuthScreen> {
     super.initState();
     _emailController.addListener(() {
       setState(() {
-        _invalidEmail = !RegExp(
-                r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
-            .hasMatch(_emailController.text);
+        if (_invalidEmail && _emailController.text.isNotEmpty) {
+          validateEmail();
+        } else if (_emailController.text.isEmpty) {
+          _invalidEmail = false;
+        }
       });
     });
     _passwordController.addListener(() {
       setState(() {
-        _invalidPassword =
-            !RegExp(r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$')
-                .hasMatch(_passwordController.text);
+        if (_invalidPassword && _passwordController.text.isNotEmpty) {
+          validatePassword();
+        } else if (_passwordController.text.isEmpty) {
+          _invalidPassword = false;
+        }
       });
     });
   }
-  //Walidate Email
+
+  bool validateInput() {
+    return validatePassword() && validateEmail();
+  }
+
+  bool validateEmail() {
+    bool valid = isValideEmail();
+    _invalidEmail = !valid;
+    return valid;
+  }
+
+  bool validatePassword() {
+    bool valid = isValidPassword();
+    _invalidPassword = !valid;
+    return valid;
+  }
+
+  bool isValideEmail() {
+    return RegExp(
+            r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+        .hasMatch(_emailController.text);
+  }
+
+  bool isValidPassword() {
+    return RegExp(r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$')
+        .hasMatch(_passwordController.text);
+  }
 
   @override
   void dispose() {
@@ -44,16 +74,22 @@ class _AuthScreenState extends State<AuthScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Вход'),
+        title: Text(
+          'Вход',
+          style: Theme.of(context).textTheme.headLiine1,
+        ),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16),
         child: Card(
+          elevation: 4.0,
           child: Padding(
             padding: const EdgeInsets.all(16),
             child: Column(
+              mainAxisSize: MainAxisSize.min,
               children: [
                 const Text('Вход'),
+                const SizedBox(height: 16),
                 buildEditText(
                   'Email',
                   _emailController,
@@ -64,8 +100,9 @@ class _AuthScreenState extends State<AuthScreen> {
                     color: Colors.white,
                   ),
                 ),
+                const SizedBox(height: 16),
                 buildEditText(
-                  'Email',
+                  'Пароль',
                   _passwordController,
                   _invalidPassword,
                   'Не корректный пароль.',
@@ -73,6 +110,20 @@ class _AuthScreenState extends State<AuthScreen> {
                     Icons.password,
                     color: Colors.white,
                   ),
+                  const SizedBox(height: 16),
+                  TextButton(
+              style: ButtonStyle(
+                  padding: MaterialStateProperty.all<EdgeInsets>(
+                      const EdgeInsets.symmetric(vertical: 8, horizontal: 16)),
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.black),
+                  foregroundColor:
+                      MaterialStateProperty.all<Color>(Colors.black12)),
+              child: Text(
+                "Регистрация",
+                style: MovieInfoSercherTheme.darkTextTheme.headline2,
+              ) 
+                    onPressed: validateInput() ? _onPressed : null,
                 )
               ],
             ),
@@ -82,11 +133,18 @@ class _AuthScreenState extends State<AuthScreen> {
     );
   }
 
+  void _onPressed() {
+    Cubit.of<AuthCubit>(context).register(
+      _emailController.text,
+      _passwordController.text,
+    );
+  }
+
   Widget buildEditText(String label, TextEditingController controller,
       bool validated, String errorMessage, Widget icon) {
-    return Row(children: [
+    return Row(crossAxisAlignment: CrossAxisAlignment.center, children: [
       Padding(
-        padding: const EdgeInsets.only(right: 8.0),
+        padding: const EdgeInsets.only(bottom: 16.0, right: 8.0),
         child: icon,
       ),
       Expanded(
